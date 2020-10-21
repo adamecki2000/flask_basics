2fc11660cf9e25a8a795ad0f9f7e2339467e770b
adamecki2000 <adamecki2000@gmail.com>
2020-10-21 19:31:52 +0200

    removed

--- classy.py
+++
@@ -1,9 +0,0 @@
-class Math:
-    notes = 'this is a class variable'
-
-    def add(self, num1, num2):
-        print('addition: ', num1+num2)
-
-    def multiply(self, num1, num2):
-        print('multiplication', num1 * num2)
-cd
